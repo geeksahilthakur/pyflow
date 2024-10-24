45ad294567215ea4b6b8e04610b95deee581ce55
Sahil Thakur <dev.sahilthakur@gmail.com>
2024-10-25 02:08:21 +0530

    Zoom fixed + also added

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 class CodeFlowExtractor(ast.NodeVisitor):
     def __init__(self):
         self.flowchart_steps = []
+        self.output_format = []
 
     def visit_FunctionDef(self, node):
         self.flowchart_steps.append({'name': f'Start Function: {node.name}', 'children': []})
+        self.output_format.append(f'__init__[__init__()]')  # Example init representation
         for n in node.body:
             self.visit(n)
 
@@ -16,6 +18,7 @@ class CodeFlowExtractor(ast.NodeVisitor):
         condition = ast.unparse(node.test)
         decision_node = {'name': f'Decision: {condition}', 'children': []}
         self.flowchart_steps[-1]['children'].append(decision_node)
+        self.output_format.append(f'Decision_0{{{condition}}}')  # Decision representation
 
         for n in node.body:
             self.visit(n)
@@ -23,17 +26,19 @@ class CodeFlowExtractor(ast.NodeVisitor):
         if node.orelse:
             else_node = {'name': 'Else', 'children': []}
             self.flowchart_steps[-1]['children'].append(else_node)
+            self.output_format.append(f'Decision_0 -->|No| Else_2[Else]')  # Else representation
             for n in node.orelse:
                 self.visit(n)
 
     def visit_Return(self, node):
         value = ast.unparse(node.value) if node.value else 'None'
         self.flowchart_steps.append({'name': f'Return: {value}', 'children': []})
+        self.output_format.append(f'Return_1[Return: {value}]')  # Return representation
 
     def extract_flow(self, code):
         tree = ast.parse(code)
         self.visit(tree)
-        return self.flowchart_steps
+        return self.flowchart_steps, '\n'.join(self.output_format)
 
 
 @app.route('/')
@@ -45,10 +50,11 @@ def home():
 def generate():
     code = request.form['code']
     extractor = CodeFlowExtractor()
-    flowchart_data = extractor.extract_flow(code)
+    flowchart_data, output_format = extractor.extract_flow(code)
 
-    # Return the flowchart data as JSON
-    return jsonify(flowchart_data)
+    # Return the flowchart data and output format as JSON
+    return jsonify(flowchart_data=flowchart_data, output_format=output_format)
+
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
